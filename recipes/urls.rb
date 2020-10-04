2d06c599498dbe29a435952fda7ede978d4e52af
Scott Richardson <dev@grogwood.com>
2020-10-04 14:22:09 -0700

    Fix to make sure each URL cache file exists before running warmer

--- recipes/urls.rb
+++ recipes/urls.rb
@@ -4,6 +4,9 @@ data_bag('sitemaps').each do |sitemap_item|
   # Load the data bag item
   sitemap = data_bag_item('sitemaps', sitemap_item)
 
+  # Skip this sitemap if no URL cache exists
+  next unless File.exist?("#{node[:olyn_warmer][:cache][:dir]}/#{sitemap[:id]}.cache")
+
   # Begin loop through each URL in our saved temporary file generated from the sitemap routine
   File.readlines("#{node[:olyn_warmer][:cache][:dir]}/#{sitemap[:id]}.cache").each do |raw_url|
 
